65d2990f82dde8e18f65b96b8ebc8b406015a6ac
kaitingtubronson <connect@kaitingtubronson.com>
2026-09-14 09:57:50 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "token-reward-board-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./token_reward_board.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
